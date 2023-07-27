b87ed8e742ef95da82d11e714e2e6f8501b97e63
omkar-2882 <omkar.gawde2002@gmail.com>
2023-07-27 10:54:01 -0400

    changed budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses, budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    console.log(totalExpenses);
+    const handleBudgetChange = (e) => {
+        if(e.value>20000){
+            alert("The value cannot exceed 20000");
+            return;
+        }
+        if(e.value<totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: e.value,
+        });
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            {/* <span>Budget: £{budget}</span> */}
+            <span>Budget: £
+                <input type="number" step="10" value = {budget} onChange={handleBudgetChange}/>
+            </span>
         </div>
     );
 };
